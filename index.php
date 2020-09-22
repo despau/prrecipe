07eb50dc6ebbad8c63e24eba352a079ef90d47f4
raug-dev24 <romanegustin24@gmail.com>
2020-09-22 12:25:33 -0400

    initial commit to recipe repository. containing only the bare minium

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:       Patronar Recipe
+ * Description:       Patronar Recipe plugin can be freely used in any projects. It Allow Users to create and rate Recipes.
+ * Version:           0.5.0
+ * Author:            Codear
+ * Text Domain:       prrecipe
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
