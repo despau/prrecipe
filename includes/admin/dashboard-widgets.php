28384b98208f41dc00c7c5da99c19c0633a63be4
raug-dev24 <romanegustin24@gmail.com>
2020-10-09 09:12:07 -0400

    created dashboard widget to display top 5 ratings

---
+++ includes/admin/dashboard-widgets.php
@@ -0,0 +1,39 @@
+<?php
+
+
+function prrecipe_dashboard_widgets(){
+
+    //(id, title, function)
+    wp_add_dashboard_widget(
+        'prrecipe_latest_recipe_rating_widget',
+        'Latest Recipe Ratings',
+        'prrecipe_latest_recipe_rating_display'
+      );
+    }
+
+    //the function
+    function prrecipe_latest_recipe_rating_display(){
+      global $wpdb;
+
+      $latest_ratings       = $wpdb->get_results(
+        "SELECT * FROM `" . $wpdb->prefix . "recipe_ratings` ORDER BY `ID` DESC LIMIT 5"
+      );
+
+      echo '<ul>';
+
+        foreach($latest_ratings as $rating){
+            $title              = get_the_title( $rating->recipe_id );
+            $permalink          = get_the_permalink( $rating->recipe_id );
+            $rating_count       = $rating->rating;
+
+            ?>
+                <li>
+                    <a href="<?php echo $permalink; ?>"><?php echo $title; ?></a>
+                    received a rating of <?php echo $rating_count; ?>
+                </li>
+            <?php
+        }
+
+      echo '</ul>';
+
+}
--- index.php
+++ index.php
@@ -43,6 +43,7 @@ include( 'process/create-account.php' );
 include( 'process/login.php' );
 include( 'includes/shortcodes/auth-alt-form.php' );
 include( 'includes/front/logout-link.php' );
+include( 'includes/admin/dashboard-widgets.php' );
 
 
 
@@ -68,7 +69,7 @@ add_action( 'wp_ajax_nopriv_prrecipe_user_login', 'prrecipe_user_login' );
 // add_filter( 'authenticate', 'prrecipe_alt_authenticate', 100, 3 );
 // add_filter( 'wp_nav_menu_items', 'patronarrecipe_new_nav_menu_items', 999 );
 add_filter( 'wp_nav_menu_secondary_items', 'patronarrecipe_new_nav_menu_items', 999 );
-// add_action( 'wp_dashboard_setup', 'r_dashboard_widgets' );
+add_action( 'wp_dashboard_setup', 'prrecipe_dashboard_widgets' );
 
 
 
